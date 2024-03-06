a97c2e1a8715eb129b508579ef50933144a2d20f
Yacin Tmimi <yacintmimi@gmail.com>
2024-03-05 23:18:12 -0500

    Lay the groundwork to consume compiler internals and parse crates
    The code here will likely change. Right now I'm not thrilled that the
    entire crate would depend on the compiler. I wonder if there's a way to
    make that optional with (probably with a feature flag). In all
    likelyhood I'll probably move the rustc specific bits to a separate
    crate so that the `markdown-fmt` crate can stay as small as possible.
    
    One thing to note; since I'm now consuming types from the compiler I
    added a rust-toolchain file to specify the nightly compiler version I
    need in order to compile my program. I'm hopeful that since I don't need
    to consume many types upgrading to newer nightly version won't be
    difficult.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,23 @@
+#![feature(rustc_private)]
+extern crate rustc_span;
+use rustc_span::edition::Edition;
+
 use clap::Parser;
 use markdown_fmt::rewrite_markdown;
+use markdown_fmt::rust_crate::parse_crate::rewrite_doc_comments_in_crate;
 use std::ffi::OsStr;
 use std::fs;
 use std::path::{Path, PathBuf};
+use std::str::FromStr;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Cli {
     /// Input file. Markdown (.md) or Rust (.rs)
     input: PathBuf,
+    /// Sets the edition value to pass when calling the rustc parser
+    #[arg(short, long, default_value="2021", value_parser = ["2015", "2018", "2021", "2024"])]
+    edition: String,
     /// Whether to emit output to stdout. Otherwise the content of the
     /// original file will be overwritten
     #[arg(long, default_value_t = false)]
@@ -26,6 +35,7 @@ fn output_result(input: &Path, result: &str, stdout: bool) -> Result<(), anyhow:
 
 fn main() -> Result<(), anyhow::Error> {
     let cli = Cli::parse();
+    let edition = Edition::from_str(&cli.edition).expect("valid edition is passed");
 
     match cli.input.extension().and_then(OsStr::to_str) {
         Some("md") => {
@@ -33,6 +43,12 @@ fn main() -> Result<(), anyhow::Error> {
             let result = rewrite_markdown(&input)?;
             output_result(&cli.input, &result, cli.stdout)
         }
+        Some("rs") => rustc_span::create_session_if_not_set_then(edition, |_| {
+            if let Some(result) = rewrite_doc_comments_in_crate(&cli.input)? {
+                return output_result(&cli.input, &result, cli.stdout);
+            }
+            Err(anyhow::anyhow!("Faile to rewrite crate doc comments"))
+        }),
         _ => Err(anyhow::anyhow!(
             "{} is not a markdown (.md) or rust (.rs) file.",
             cli.input.display()
