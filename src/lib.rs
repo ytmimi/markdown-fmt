efcf1617d44d86d1a12e6446536894c746c2627c
Yacin Tmimi <yacintmimi@gmail.com>
2024-03-17 23:35:37 -0400

    Revert commit that consumes compiler internals (PR 3)
    This reverts commit 6331d65a39c57ff8f3f40e7143c07f14626e0f7e.
    
    After thinking about it a little more I think this change was made too
    early. I eventually want to provide some way to do this, but I don't
    want the markdown formatting crate to directly depend on rustc.

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,8 @@
-#![feature(rustc_private)]
-
-// N.B. these crates are loaded from the sysroot, so they need extern crate.
-extern crate rustc_ast;
-extern crate rustc_ast_pretty;
-extern crate rustc_builtin_macros;
-extern crate rustc_data_structures;
-extern crate rustc_errors;
-extern crate rustc_expand;
-extern crate rustc_parse;
-extern crate rustc_session;
-extern crate rustc_span;
-extern crate thin_vec;
-
-// Necessary to pull in object code as the rest of the rustc crates are shipped only as rmeta
-// files.
-#[allow(unused_extern_crates)]
-extern crate rustc_driver;
-
 mod builder;
 mod escape;
 mod formatter;
 mod links;
 mod list;
-pub mod rust_crate;
 mod table;
 mod utils;
 
