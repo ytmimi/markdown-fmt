ae0325fc956e2ea7edbf389fb16de8268522f5a3
Yacin Tmimi <yacintmimi@gmail.com>
2024-03-17 23:35:37 -0400

    Revert commit that consumes compiler internals (PR 3)
    This reverts commit 6331d65a39c57ff8f3f40e7143c07f14626e0f7e.
    
    After thinking about it a little more I think this change was made too
    early. I eventually want to provide some way to do this, but I don't
    want the markdown formatting crate to directly depend on rustc.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,14 @@
-#![feature(rustc_private)]
-
-extern crate rustc_span;
 use clap::Parser;
 use markdown_fmt::rewrite_markdown;
-use markdown_fmt::rust_crate::parse_crate::rewrite_doc_comments_in_crate;
-use rustc_span::edition::Edition;
 use std::ffi::OsStr;
 use std::fs;
 use std::path::{Path, PathBuf};
-use std::str::FromStr;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Cli {
     /// Input file. Markdown (.md) or Rust (.rs)
     input: PathBuf,
-    /// Sets the edition value to pass when calling the rustc parser
-    #[arg(short, long, default_value="2021", value_parser = ["2015", "2018", "2021", "2024"])]
-    edition: String,
     /// Whether to emit output to stdout. Otherwise the content of the
     /// original file will be overwritten
     #[arg(long, default_value_t = false)]
@@ -35,15 +26,8 @@ fn output_result(input: &Path, result: &str, stdout: bool) -> Result<(), anyhow:
 
 fn main() -> Result<(), anyhow::Error> {
     let cli = Cli::parse();
-    let edition = Edition::from_str(&cli.edition).expect("valid edition is passed");
 
     match cli.input.extension().and_then(OsStr::to_str) {
-        Some("rs") => rustc_span::create_session_if_not_set_then(edition, |_| {
-            if let Some(result) = rewrite_doc_comments_in_crate(&cli.input)? {
-                return output_result(&cli.input, &result, cli.stdout);
-            }
-            Err(anyhow::anyhow!("Faile to rewrite crate doc comments"))
-        }),
         Some("md") => {
             let input = fs::read_to_string(&cli.input)?;
             let result = rewrite_markdown(&input)?;
