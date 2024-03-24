3e69a02421f23783040cf4144afbd812015da337
Yacin Tmimi <yacintmimi@gmail.com>
2024-03-24 11:03:35 -0400

    Add crate level documentation to lib.rs
    Provide some very basic examples of using the `rewrite_markdown` and
    `rewrite_markdown_with_builder` functions.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,68 @@
+//! Easily format Markdown. [markdown_fmt] supports [CommonMark] and [GitHub Flavored Markdown].
+//!
+//! [markdown_fmt]: index.html
+//! [CommonMark]: https://spec.commonmark.org/
+//! [GitHub Flavored Markdown]: https://github.github.com/gfm/
+//!
+//! # Getting Started
+//!
+//! ```rust
+//! use markdown_fmt::rewrite_markdown;
+//!
+//! let markdown = r##" # Getting Started
+//! 1. numbered lists
+//! 1.  are easy!
+//! "##;
+//!
+//! let formatted = r##"# Getting Started
+//! 1. numbered lists
+//! 2. are easy!"##;
+//!
+//! let output = rewrite_markdown(markdown)?;
+//! # assert_eq!(output, formatted);
+//! # Ok::<(), std::io::Error>(())
+//! ```
+//!
+//! # Using the [Builder](builder::FormatterBuilder)
+//!
+//! The builder gives you more control to configure Markdown formatting.
+//!
+//! ````rust
+//! use markdown_fmt::{rewrite_markdown, rewrite_markdown_with_builder, FormatterBuilder};
+//!
+//! let builder = FormatterBuilder::with_code_block_formatter(|info_string, code_block| {
+//!     match info_string.to_lowercase().as_str() {
+//!         "markdown" => rewrite_markdown(&code_block).unwrap_or(code_block),
+//!         _ => code_block
+//!     }
+//! });
+//!
+//! let markdown = r##" # Using the Builder
+//! + markdown code block nested in a list
+//!   ```markdown
+//!   A nested markdown snippet!
+//!
+//!    * unordered lists
+//!    are also pretty easy!
+//!    - `-` or `+` can also be used as unordered list markers.
+//!    ```
+//! "##;
+//!
+//! let formatted = r##"# Using the Builder
+//! + markdown code block nested in a list
+//!   ```markdown
+//!   A nested markdown snippet!
+//!
+//!   * unordered lists
+//!     are also pretty easy!
+//!   - `-` or `+` can also be used as unordered list markers.
+//!   ```"##;
+//!
+//! let output = rewrite_markdown_with_builder(markdown, builder)?;
+//! # assert_eq!(output, formatted);
+//! # Ok::<(), std::io::Error>(())
+//! ````
+
 mod builder;
 mod escape;
 mod formatter;
