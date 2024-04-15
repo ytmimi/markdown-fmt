1a8e8b28da3a1e6d58b80595ff5c50f4ae698e6d
Yacin Tmimi <yacintmimi@gmail.com>
2024-04-15 02:21:29 -0400

    Allow users to configure the `max_width`
    Now the CLI exposes a `max_width` option!

--- src/config.rs
+++ src/config.rs
@@ -2,3 +2,13 @@
 pub(crate) struct Config {
     max_width: Option<usize>,
 }
+
+impl Config {
+    pub(crate) fn max_width(&self) -> Option<usize> {
+        self.max_width
+    }
+
+    pub(crate) fn set_max_width(&mut self, value: Option<usize>) {
+        self.max_width = value;
+    }
+}
