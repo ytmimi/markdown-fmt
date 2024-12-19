88173249c4d846c474dcc4c51de1e8fc49e64bea
Yacin Tmimi <yacintmimi@gmail.com>
2024-12-18 21:31:02 -0500

    fix: update the approach to escape HTML block tag-like text

--- src/html.rs
+++ src/html.rs
@@ -50,3 +50,21 @@ pub(crate) static HTML_BLOCK_TAG: &[&str] = &[
     "script",
     "style",
 ];
+
+/// Check if the given string starts with a well known HTML block tag name
+pub(crate) fn starts_with_html_block_identifier(s: &str) -> bool {
+    if s.trim().is_empty() || !s.is_ascii() {
+        return false;
+    }
+
+    let is_html_block_identifier = |value: &str| {
+        HTML_BLOCK_TAG
+            .iter()
+            .any(|tag| tag.eq_ignore_ascii_case(value))
+    };
+
+    match s.split_whitespace().next() {
+        Some(t) => is_html_block_identifier(t),
+        None => is_html_block_identifier(s),
+    }
+}
